b5270acfab978a7ae718359e65e1d139923f0dba
jparrker <102701917+jparrker@users.noreply.github.com>
2023-04-16 19:37:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple app using useState and useEffect hooks to fetch data and render a list of slides of employees. Carousel functionality implemented by using useState to keep track of current slide and changing index based on onclick events for next and previous.
+
+Reimplemented the carousel using popular library react slick.
---
+++ src/Carousel.jsx
@@ -0,0 +1,64 @@
+import { shortList, list, longList } from "./data";
+import { useEffect, useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+function Carousel() {
+  const [people, setPeople] = useState(longList);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentPerson((old) => {
+      const result = (old - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrentPerson((old) => {
+      const result = (old + 1) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => {
+      clearInterval(sliderId);
+    };
+  }, [currentPerson]);
+
+  return (
+    <section className="slider-container">
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? "visible" : "hidden",
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+}
+
+export default Carousel;
